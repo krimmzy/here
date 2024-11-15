e507a1aaad4199a56e49d2713b7f1bd88e697915
Akorede Lemo <125971535+krimmzy@users.noreply.github.com>
2024-11-15 22:48:09 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('.faq-question').forEach(button => {
+    button.addEventListener('click', () => {
+        const answer = button.nextElementSibling;
+        button.classList.toggle('active');
+        answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+    });
+});
